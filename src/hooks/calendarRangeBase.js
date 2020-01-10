9ac614dbaf96ccc6aba1590b5d365cae079fe4d2
mohsen <mohsen@saeeds-MacBook-Pro.local>
2020-01-10 22:34:53 +0330

    add calendarRangeBase.js

---
+++ src/hooks/calendarRangeBase.js
@@ -0,0 +1,19 @@
+import {useDatePickerContext} from "./index";
+
+export const useCalendarRangeBase = (props) => {
+
+    const {
+        value,
+        format,
+    } = props;
+
+
+    const {adapter} = useDatePickerContext();
+    const text1 = value[0] && adapter.isValid(value[0]) && adapter.formatByString(value[0], format || adapter.formats.fullDate);
+    const text2 = value[1] && adapter.isValid(value[1]) && adapter.formatByString(value[1], format || adapter.formats.fullDate);
+
+    return {
+        text1: text1 || "",
+        text2: text2 || "",
+    }
+};
