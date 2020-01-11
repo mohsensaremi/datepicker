418e7848ba94f0b705d055dcf04b65e914a7cd16
mohsen <mohsen@saeeds-MacBook-Pro.local>
2020-01-11 19:01:37 +0330

    use moment direct format function instead of date-io format because of moment set locale bug

--- src/hooks/calendarRangeInputBase.js
+++ src/hooks/calendarRangeInputBase.js
@@ -9,8 +9,8 @@ export const useCalendarRangeInputBase = (props) => {
 
 
     const {adapter} = useDatePickerContext();
-    const text1 = value[0] && adapter.isValid(value[0]) && adapter.formatByString(value[0], format || adapter.formats.fullDate);
-    const text2 = value[1] && adapter.isValid(value[1]) && adapter.formatByString(value[1], format || adapter.formats.fullDate);
+    const text1 = value[0] && adapter.isValid(value[0]) && value[0].format(format || adapter.formats.fullDate);
+    const text2 = value[1] && adapter.isValid(value[1]) && value[1].format(format || adapter.formats.fullDate);
 
     return {
         text1: text1 || "",
