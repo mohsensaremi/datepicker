430f7dacfbfe81a415b2c9a78daa92a3c66d281f
mohsen <mohsen@saeeds-MacBook-Pro.local>
2020-01-12 09:39:25 +0330

    fix today

--- src/hooks/day.js
+++ src/hooks/day.js
@@ -10,12 +10,12 @@ export const useDay = (props) => {
 
     const {adapter} = useDatePickerContext();
     const today = adapter.date();
-    const isToday = adapter.isEqual(date, today);
+    const isToday = adapter.isEqual(adapter.startOfDay(date), adapter.startOfDay(today));
     const isActive = adapter.isValid(value) && adapter.isEqual(date, value);
     const isPast = adapter.isBefore(date, adapter.addDays(today, -1));
     const onClick = () => onChange(date);
     const text = adapter.format(date, 'dayOfMonth');
-
+    
     return {
         isToday,
         isActive,
