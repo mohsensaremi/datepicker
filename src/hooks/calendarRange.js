8c1fdd22e98d0d7956d6ecbf3b33f15952df1347
mohsen <mohsen@saeeds-MacBook-Pro.local>
2020-01-13 19:08:09 +0330

    create local value for range calendar and update value after full selection of date

--- src/hooks/calendarRange.js
+++ src/hooks/calendarRange.js
@@ -1,17 +1,25 @@
 import {useCalendar} from "./calendar";
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 export const useCalendarRange = (props) => {
 
     const {
         value,
+        onChange,
     } = props;
 
     const calendarHooks = useCalendar(props);
+    const [localValue, localOnChange] = useState(value);
     const [hoverDate, setHoverDate] = useState(null);
 
+    useEffect(() => {
+        if (localValue.length === 2) {
+            onChange(localValue);
+        }
+    }, [value, onChange, localValue, localOnChange]);
+
     const onMouseEnterDay = (date) => {
-        if (value.length === 1) {
+        if (localValue.length === 1) {
             setHoverDate(date);
         }
     };
@@ -21,5 +29,9 @@ export const useCalendarRange = (props) => {
         ...calendarHooks,
         onMouseEnterDay,
         hoverDate,
+        localValue,
+        localOnChange,
+        value: localValue,
+        onChange: localOnChange,
     };
 };
